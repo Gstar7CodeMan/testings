f2cb2d80a2789473039879af03feb886e847a4f7
Gstar7CodeMan <71800085+Gstar7CodeMan@users.noreply.github.com>
2023-06-22 18:04:17 -0400

    Update js.py

--- js.py
+++ js.py
@@ -2,24 +2,26 @@ from django.db.models import F, Case, When
 from your_app.models import gdata
 
 def update_fields_with_conversion():
-    field_mapping = {
-        'security_review_rating': {
-            'grade_range': [(1.0, 2.0, 'A'), (2.0, 3.0, 'B'), (3.0, 4.0, 'C'), (4.0, 5.0, 'D'), (5.0, 6.0, 'E')],
-        },
-        'field2': {
-            'grade_range': [(0.0, 1.0, 'X'), (1.0, 2.0, 'Y'), (2.0, 3.0, 'Z')],
-        },
-        # Add more fields and their grade ranges here
+    grade_mapping = {
+        1.0: 'A',
+        2.0: 'B',
+        3.0: 'C',
+        4.0: 'D',
+        5.0: 'E',
     }
 
-    for field_name, field_info in field_mapping.items():
-        grade_ranges = field_info['grade_range']
+    field_mapping = {
+        'security_review_rating': grade_mapping,
+        'field2': grade_mapping,
+        # Add more fields here with the same grade mapping
+    }
+
+    for field_name, grade_mapping in field_mapping.items():
         cases = []
 
-        for min_value, max_value, grade in grade_ranges:
+        for value, grade in grade_mapping.items():
             cases.append(When(**{
-                field_name + '__gte': min_value,
-                field_name + '__lt': max_value,
+                field_name: value,
                 'then': grade,
             }))
 
