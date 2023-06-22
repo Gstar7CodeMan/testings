6aa71bc99ede1365d985212a3225f59686a06103
Gstar7CodeMan <71800085+Gstar7CodeMan@users.noreply.github.com>
2023-06-22 18:41:17 -0400

    Update js.py

--- js.py
+++ js.py
@@ -1,6 +1,6 @@
 from django.db.models import F, Case, When, Value
-from django.db.models.functions import Cast
-from your_app.models import gdata
+from django.db.models.fields import CharField
+from your_app.models import Model1, Model2
 
 def update_fields_with_conversion():
     grade_mapping = {
@@ -11,21 +11,42 @@ def update_fields_with_conversion():
         5.0: 'E',
     }
 
-    field_mapping = {
+    field_mapping_model1 = {
         'security_review_rating': grade_mapping,
         'field2': grade_mapping,
-        # Add more fields here with the same grade mapping
+        # Add more fields for Model1 here
     }
 
-    for field_name, grade_mapping in field_mapping.items():
+    field_mapping_model2 = {
+        'field3': grade_mapping,
+        'field4': grade_mapping,
+        # Add more fields for Model2 here
+    }
+
+    # Update fields for Model1
+    for field_name, grade_mapping in field_mapping_model1.items():
         cases = []
 
         for value, grade in grade_mapping.items():
             cases.append(When(**{
-                field_name: value,
+                field_name + '__exact': value,
                 'then': Value(grade, output_field=CharField()),
             }))
 
-        gdata.objects.update(
+        Model1.objects.update(
+            **{field_name: Case(*cases, default=F(field_name), output_field=CharField())}
+        )
+
+    # Update fields for Model2
+    for field_name, grade_mapping in field_mapping_model2.items():
+        cases = []
+
+        for value, grade in grade_mapping.items():
+            cases.append(When(**{
+                field_name + '__exact': value,
+                'then': Value(grade, output_field=CharField()),
+            }))
+
+        Model2.objects.update(
             **{field_name: Case(*cases, default=F(field_name), output_field=CharField())}
         )
